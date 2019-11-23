093f72d35e2999bd9adba9cf6409cd6b5319d53e
tangweikun <819573105@qq.com>
2019-11-23 14:48:22 +0800

    :construction: add icon

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
+import CHEVRON_LEFT from "./assets/chevron-left.svg";
+import CHEVRON_RIGHT from "./assets/chevron-right.svg";
 import "./Foo.scss";
 
 export function Footer(props) {
   return (
     <div className="Footer" data-step={props.step}>
       <div className="Button Button--prev" onClick={props.decreaseStep}>
-        <i className="fa fa-fw fa-chevron-left" />
+        <img src={CHEVRON_LEFT} alt="chevron-left" />
       </div>
       <div className="Dots" data-step={props.step}>
         <div className="Dot" />
@@ -13,7 +15,7 @@ export function Footer(props) {
         <div className="Dot" />
       </div>
       <div className="Button Button--next" onClick={props.increaseStep}>
-        <i className="fa fa-fw fa-chevron-right" />
+        <img src={CHEVRON_RIGHT} alt="chevron-right" />
       </div>
     </div>
   );
--- src/Slide.js
+++ src/Slide.js
@@ -4,7 +4,6 @@ import "./Foo.scss";
 export function Slide(props) {
   return (
     <div className="Slide">
-      <i className={"fa fa-fw fa-" + props.icon} />
       <div className="Text">{props.text}</div>
     </div>
   );
