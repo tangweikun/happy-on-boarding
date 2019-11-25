faf5e5bc7187f790c2abed0c85728056d59608b6
tangweikun <819573105@qq.com>
2019-11-25 15:16:08 +0800

    Updates

---
+++ precache-manifest.b8aeb43547da171b36124154a867ad23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037e426d2a79ee8b991b62a156b796b7",
+    "url": "/happy-on-boarding/index.html"
+  },
+  {
+    "revision": "611b0bd40cd5a1be504f",
+    "url": "/happy-on-boarding/static/css/main.673aaa39.chunk.css"
+  },
+  {
+    "revision": "71af543c4a8a1165e09a",
+    "url": "/happy-on-boarding/static/js/2.1dd51b37.chunk.js"
+  },
+  {
+    "revision": "611b0bd40cd5a1be504f",
+    "url": "/happy-on-boarding/static/js/main.a87f748f.chunk.js"
+  },
+  {
+    "revision": "1ef6ebffd8c9c0075791",
+    "url": "/happy-on-boarding/static/js/runtime-main.6bee9d49.js"
+  },
+  {
+    "revision": "e96af375a87380eeb094d3073da14dcd",
+    "url": "/happy-on-boarding/static/media/chevron-left.e96af375.svg"
+  },
+  {
+    "revision": "0d29f83cc09a173fff24fa38ce4b3de8",
+    "url": "/happy-on-boarding/static/media/chevron-right.0d29f83c.svg"
+  }
+]);
